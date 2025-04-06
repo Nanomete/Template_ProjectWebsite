2a3bd9fdf699fab5227249d1a4bf479b53e39270
Nawamin ICE <157040537+Nanomete@users.noreply.github.com>
2025-04-06 16:47:19 +0700

    add folder project

---
+++ My_App/src/Component/Footer.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <footer class="text-center p-4">
+        <div class="container">
+            <div class="row">
+                <div class="col">
+                    <p class="text-light"> Copyright &copy; Nanomete Official</p>
+                </div>
+            </div>
+        </div>
+    </footer>
+    )
+}
+
+export default Footer
